6f129ec1adb89512d3f032b9dd6c298a4c883760
Pradikshan <pradikshan234@gmail.com>
2025-01-24 20:12:50 +0530

    Made minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,34 @@ function App() {
   return (
     <>
       <div>
+        <Banner
+          text="hello there"
+          animation="carousel"
+          textStyles="font-bold"
+          backgroundColor="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500"
+          duration={15}
+          closeIconVisible={true}
+        />
         <p>hello there</p>
-        <Banner text="Hello there" textArray={arr} />
+        <Banner
+          textArray={arr}
+          animation="carousel"
+          textStyles="font-bold mx-10"
+        />
+        <p>hello there</p>
+        <Banner
+          text="Hello there"
+          duration={1.5}
+          animation="verticalCarousel"
+          textStyles="font-bold text-center"
+        />
+        <p>hello there</p>
+        <Banner
+          textArray={arr}
+          duration={2}
+          animation="verticalCarousel"
+          textStyles="font-bold mx-10"
+        />
       </div>
     </>
   );

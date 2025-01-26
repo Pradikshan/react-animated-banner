a098473a8ab9326e63834e9115e779fdb39203e1
Pradikshan <pradikshan234@gmail.com>
2025-01-26 17:10:42 +0530

    Extracted vertical carousel animation logic to a seperate file

---
+++ src/animationConfigs/verticalCarousel.ts
@@ -0,0 +1,18 @@
+export const verticalCarouselAnimationConfig = (
+  animationDuration?: number,
+  direction?: string
+) => ({
+  initial: { y: direction === "reverse" ? "-100%" : "100%" },
+  animate: {
+    y:
+      direction === "reverse"
+        ? ["-100%", "0%", "0%", "100%"]
+        : ["100%", "0%", "0%", "-100%"],
+  },
+  transition: {
+    repeat: Infinity,
+    duration: animationDuration,
+    ease: "linear",
+    times: [0, 0.4, 0.6, 1],
+  },
+});

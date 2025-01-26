9cd771e885b76c274b9577ca05e6602895ffec52
Pradikshan <pradikshan234@gmail.com>
2025-01-26 17:10:48 +0530

    Extracted carousel animation logic to a seperate file

---
+++ src/animationConfigs/carousel.ts
@@ -0,0 +1,12 @@
+export const carouselAnimationConfig = (
+  animationDuration?: number,
+  direction?: string
+) => ({
+  initial: { x: direction === "reverse" ? "-100%" : "100%" },
+  animate: { x: direction === "reverse" ? "100%" : "-100%" },
+  transition: {
+    repeat: Infinity,
+    duration: animationDuration,
+    ease: "linear",
+  },
+});
